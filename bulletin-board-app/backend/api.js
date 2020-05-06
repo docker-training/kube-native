f3cda5466bb9ecdfb3733c4b23f64cd9577a4de0
Bill Mills <mills.wj@gmail.com>
2020-05-06 13:35:17 -0400

    Update api.js

--- bulletin-board-app/backend/api.js
+++ bulletin-board-app/backend/api.js
@@ -2,11 +2,11 @@ var db = require('./db.js');
 const log = require('log-to-file');
 
 exports.events = function (req, res) {
-  log('Loading DB events...', '/api.log');
+  log('Loading DB events...', '/logs/api.log');
   db.Events
     .findAll()
     .then(events => {
-        log('Fetched events, count: ' + events.length, '/api.log');
+        log('Fetched events, count: ' + events.length, '/logs/api.log');
         res.json(events);
     })
     .catch(err => {
@@ -15,7 +15,7 @@ exports.events = function (req, res) {
 };
 
 exports.event = function (req, res) {
-  log('Handling event call, method: ' + req.method + ', event ID: ' + req.params.eventId, '/api.log')
+  log('Handling event call, method: ' + req.method + ', event ID: ' + req.params.eventId, '/logs/api.log')
   switch(req.method) {
     case "DELETE":
       db.Events
@@ -24,7 +24,7 @@ exports.event = function (req, res) {
           id: req.params.eventId
         }
       }).then(function() {
-        log('Deleted event with id: ' + req.params.eventId, '/api.log')
+        log('Deleted event with id: ' + req.params.eventId, '/logs/api.log')
         res.status(200).end();
       });
       break
